d607e40f808e05036845f3255c4e0d5263ba7460
TeoMunoz <teo.munoz.blasco@gmail.com>
2022-10-25 11:47:15 +0200

    Index + nach kontrolieren das alles fungcioniert.

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @OA\Info(title="OnlineShop", version="1.0")
+ */
+
+use Psr\Http\Message\ResponseInterface as Response;
+	use Psr\Http\Message\ServerRequestInterface as Request;
+	use Slim\Factory\AppFactory;
+	use ReallySimpleJWT\Token;
+
+	require __DIR__ . "/../vendor/autoload.php";
+	require "model/registration.php";
+	require_once "config/config.php";
+
+    $app = AppFactory::create();
+
+	function error($message, $code) {
+		//Make the error to a Json.
+		$error = array("message" => $message);
+		echo json_encode($error);
+
+		//The error Code
+		http_response_code($code);
+
+		die();
+	}
+	$app->run();
+
+?>
